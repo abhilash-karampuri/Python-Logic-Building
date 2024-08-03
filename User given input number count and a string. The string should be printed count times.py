36edd7a95acf0c8f5325adffcd5d0c39dc087a4e
Abhilash Karampuri <abhilashkarampuri58@gmail.com>
2024-08-03 20:52:18 +0530

    Add files via upload

---
+++ User given input number count and a string. The string should be printed count times.py
@@ -0,0 +1,6 @@
+#User given input number count and a string. The string should be printed count times.
+def result(str):
+    print(str*count)
+count=int(input("Enter number of times:"))
+str=input("Enter string")
+result(str);
