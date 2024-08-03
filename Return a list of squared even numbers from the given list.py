30ff5dc9c3f8657095f7ebb93b0e52347098808f
Abhilash Karampuri <abhilashkarampuri58@gmail.com>
2024-08-03 20:56:42 +0530

    Update Return a list of squared even numbers from the given list.py

--- Return a list of squared even numbers from the given list.py
+++ Return a list of squared even numbers from the given list.py
@@ -1,8 +1,9 @@
 #Write a Python function that takes a list of integers and returns a new list containing only the even numbers from the original list, squared
-arr=[1,2,3,4,5,6]
-newarray=[]
-for x in range(1,7):
-    if x%2==0:
-        square=x*x
-        newarray.append(square);
-print(newarray)
+def squarearray(newarray):
+    newarray=[]
+    for x in arr:
+        if x%2==0:
+            newarray.append(x*x)
+    return newarray
+arr=[12,23,45,78]
+squarearray(newarray)
