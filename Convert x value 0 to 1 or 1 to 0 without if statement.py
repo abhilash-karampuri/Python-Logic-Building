0f5a0ce9ce5db0fd73bd0beb2f648dd09a343842
Abhilash Karampuri <abhilashkarampuri58@gmail.com>
2024-08-03 22:59:02 +0530

    Update Convert x value 0 to 1 or 1 to 0 without if statement.py

--- Convert x value 0 to 1 or 1 to 0 without if statement.py
+++ Convert x value 0 to 1 or 1 to 0 without if statement.py
@@ -1,6 +1,10 @@
 #a variable x has either value 0 or 1. Convert x to 1 if its 0 and if its 1 convert to 0. Note you cannot use if statement
 x=int(input())
-#x=(x+1)%2
-#x=1-x
-x=not x and 1
+x=1-x
 print(x)
+'''
+Method2:
+x=not x and 1
+Method3:
+#x=(x+1)%2
+'''
